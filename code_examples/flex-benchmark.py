491480bd1b421827991589996871905cb3c90c8c
Fatih Gülakar <gulakar15@itu.edu.tr>
2019-10-24 18:44:39 +0300

    Add files via upload

--- code_examples/flex-benchmark.py
+++ code_examples/flex-benchmark.py
@@ -3,16 +3,13 @@ Fast prototyping for NN architecture benchmark. Modify the class NN,
 optimizer and loss function (if needed) and script will calculate 
 training time and accuracy. Then it will write all the data to an Excel file.
 TODO :
-    1. Append results to Google Sheets instead of local Excel file. (or check
-internet connectivity, if it fails, write to local Excel file.)
-    2. Graphical results using matplotlib
+    1. Graphical results using matplotlib
 
-
-WARNING: Program requires "openpyxl" package,
-in Anaconda type "conda install -c anaconda openpyxl" to install it
+WARNING: Program requires "openpyxl", "gspread" and "oauth2client" packages,
+in Anaconda type "conda install -c anaconda openpyxl gspread oauth2client"
+to install them
 
 Author: Mehmet Fatih Gülakar - 19/10/2019
-
 """
 import torch
 import torch.nn as nn
@@ -25,6 +22,8 @@ import time
 import platform
 import openpyxl
 from datetime import date
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 # Change this if needed.
 batchSize = 4
@@ -37,7 +36,8 @@ testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, 
 testloader = torch.utils.data.DataLoader(testset, batch_size=batchSize, shuffle=False, num_workers=0)
 classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
 
-epochs = 1
+# Change if needed.
+epochs = 10
 
 # Change the NN architecture for benchmarking.
 class Net(nn.Module):
@@ -58,6 +58,7 @@ optimizer = torch.optim.SGD(net.parameters(), lr=1e-2, momentum=0.9)
 start_time = time.time()
 
 # Train
+print("-->Training begins")
 for epoch in range(epochs):
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
@@ -90,27 +91,39 @@ with torch.no_grad():
 
 accuracy = float(100*correct/total)
 architecture_description = "Referring to a text file here might be helpful"
-simulation_is_made_by = "Who made the simulation? (e.g. John Smith)"
+training_is_made_by = "Who made the training? (e.g. John Smith)"
 LossFunction = criterion.__class__.__name__
 optimizerName = optimizer.__class__.__name__
 dataset = trainset.__class__.__name__
 time_added = date.today().strftime("%d/%m/%Y")
 
-toWrite = [architecture_description, LossFunction, optimizerName, dataset, batchSize,
-            devicename, accuracy, elapsed_time, time_added, simulation_is_made_by]
+toWrite = [architecture_description, LossFunction, optimizerName, dataset, batchSize, epochs,
+            devicename, accuracy, elapsed_time, time_added, training_is_made_by]
 
-# Try to load Benchmark.xlsx. If it fails, create a new one, write column names, and append the simulation results.
+# Try to connect Google sheets. If connected, append row data to sheet.
 try:
-    workbook = openpyxl.load_workbook("Benchmark.xlsx")
-except:
-    workbook = openpyxl.Workbook()
-    worksheet = workbook.worksheets[0]
-    worksheet.append(["Description","Loss Function", "Optimizer", "Dataset", "Batch Size", "Device Name", 
-                        "Accuracy(%)", "Training Time(min)", "Simulation Date", "Simulation is done by"])
-# Select first worksheet
-worksheet = workbook.worksheets[0]
-worksheet.append(toWrite)
-workbook.save("Benchmark.xlsx")
+    scope = ["https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    client = gspread.authorize(creds)
+    worksheet = client.open("Benchmark").sheet1
+    worksheet.append_row(toWrite)
+    print("Results are imported to Google sheets successfully.")
 
+# If cannot connect due to Internet or problem at Google.
+except:
+    # Try to open file
+    try:
+        workbook = openpyxl.load_workbook("Benchmark.xlsx")
+    # If "Benchmark.xlsx" does not exist, create a new one.
+    except:
+        workbook = openpyxl.Workbook()
+        worksheet = workbook.worksheets[0]
+        worksheet.append(["Description","Loss Function", "Optimizer", "Dataset", "Batch Size", "Epoch", "Device Name", 
+                        "Accuracy(%)", "Training duration(min)", "Training Date", "Training is done by"])
+    # Select first worksheet and append the data, then save the file.
+    worksheet = workbook.worksheets[0]
+    worksheet.append(toWrite)
+    workbook.save("Benchmark.xlsx")
+    print("Results are imported to local Excel file successfully.")
 
 
