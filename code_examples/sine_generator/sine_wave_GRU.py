6b9bf8666ccb00d7dfde74568d9ff217a93eaa5c
Mehmet Kapson <56561848+Bladrach@users.noreply.github.com>
2019-12-23 20:10:44 +0300

    Update sine_wave_GRU.py

--- code_examples/sine_generator/sine_wave_GRU.py
+++ code_examples/sine_generator/sine_wave_GRU.py
@@ -10,7 +10,7 @@ import random
 # signal variables
 sampling_freq = 100                
 signal_duration = 2
-total_samples = sampling_freq*signal_duration
+total_samples = sampling_freq * signal_duration
 
 # learning variables
 max_epoch = 150 + 1
@@ -21,11 +21,11 @@ seq_length = 5
 
 # GRU parameters 
 num_layers = 3
-hidden_size = 15
+hidden_size = 3
 input_size = 1
 size_out = input_size
 
-x=torch.linspace(0, signal_duration*2*np.pi, total_samples)  # dividing signal_duration * 2 * np.pi interval into (total_samples) pieces with same length, x data
+x=torch.linspace(0, signal_duration * 2 * np.pi, total_samples)  # dividing signal_duration * 2 * np.pi interval into (total_samples) pieces with same length, x data
 y_gt = torch.sin(x)  # create sine wave for x points, y data
 
 print(x.size(),y_gt.size())  # print sizes of x and y_gt tensors
@@ -59,15 +59,15 @@ for epoch in range(max_epoch):
 
     # train loop
     for epoch_index in range(total_samples):            
-
+        h_state = torch.zeros(num_layers, batch_size, hidden_size)
         input_batch =  torch.zeros(seq_length, batch_size, 1)  # create tensor of zeros with batch_size as its size to use as input
-        ground_truth_batch = torch.zeros(seq_length, batch_size, 1)  # create tensor of zeros with batch_size as its size to use as ground truth
+        ground_truth_batch = torch.zeros(1, batch_size, 1)  # create tensor of zeros with batch_size as its size to use as ground truth
 
         for batch_index in range(batch_size):  # loop for getting random pieces 
             rand_index = random.randint(0, total_samples - 1 - seq_length)   # generate random int between 0 and total_samples-1
             input_batch[:, batch_index, 0] = x[rand_index : rand_index + seq_length]  # copy a portion of it that has size of seq_length to the input_batch
                                                                                       # use random sort order to avoid optimizer be stuck on local minimum
-            ground_truth_batch[:, batch_index, 0] = y_gt[rand_index : rand_index+seq_length]  # assign the same random element of y_gt to ground truth tensor's element 
+            ground_truth_batch[0, batch_index, 0] = y_gt[rand_index+seq_length]  # assign the same random element of y_gt to ground truth tensor's element 
 
         out, h_state = model(input_batch, h_state)  # prediction and new h_state of model for input_batch and h_state                  
         h_state.detach_()
@@ -80,17 +80,17 @@ for epoch in range(max_epoch):
         print("Epoch: ", str(epoch), " Iteration: ", str(epoch_index), " Loss: ", str(loss.data))
        
     # in the training we took pieces ,but in the test we want to test all xs and ys 
-    h_state = torch.zeros(num_layers, batch_size, hidden_size)  # initialize fresh hidden state to use in test loop
     # test loop
     if epoch % test_number == 0:  # put every 'test_number' of epochs to test loop
-        out_buffer = torch.zeros(total_samples)  # create tensor of zeros with total_samples as its size to use as out_buffer 
-        for iteration_index in range(int(total_samples / (seq_length * batch_size))):
-            
-            out, h_state = model(x[iteration_index * seq_length * batch_size : (iteration_index + 1) * seq_length * batch_size].view(seq_length, batch_size, -1), h_state)
-            out = out.view(-1, 1)[:, 0]
 
-            out_buffer[iteration_index * seq_length * batch_size : (iteration_index + 1) * seq_length * batch_size] = out                
-            loss = loss_function(y_gt[iteration_index * seq_length * batch_size : (iteration_index + 1) * seq_length * batch_size], out)
+        out_buffer = torch.zeros(total_samples)  # create tensor of zeros with total_samples as its size to use as out_buffer 
+        for iteration_index in range(seq_length, total_samples):
+            h_state = torch.zeros(num_layers, 1, hidden_size)  # initialize fresh hidden state to use in test loop
+
+            out, h_state = model( x[iteration_index - seq_length + 1 : iteration_index + 1].view(seq_length, 1, -1), h_state)
+
+            out_buffer[iteration_index] = out[0, 0, 0]                
+            loss = loss_function(y_gt[iteration_index], out)
 
             # print epoch, iteration and its loss
             print("Epoch: ", str(epoch), " Iteration: ", str(iteration_index), " Loss: ", str(loss.data))
