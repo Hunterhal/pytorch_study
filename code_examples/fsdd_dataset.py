dc35f40f725538e2345f9314da5f7c87543f7179
Fatih Gülakar <gulakar15@itu.edu.tr>
2020-01-31 17:31:11 +0300

    Spectrogram Learner
    Learning sound digits using LSTM. Download Free-spoken-digits-dataset (https://github.com/Jakobovski/free-spoken-digit-dataset). Then change folder_data variable in learn-spec.py according to the directory of FSDD dataset. For NN model, please don't forget to use models.py, located in /sine_generator directory in this repository.

--- code_examples/fsdd_dataset.py
+++ code_examples/fsdd_dataset.py
@@ -1,18 +1,7 @@
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
 import torch
-import torch.nn as nn
-import numpy as np
+import torchaudio
+from torch.utils.data import Dataset
 import glob
-from scipy.io import wavfile
-import models
-
-
-# Change this
-folder_data = glob.glob("C:\\Users\\fatih\\grad\\free-spoken-digit-dataset\\recordings\\*.wav")
-
-classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
-
 
 class MyCustomFSDD(Dataset):
     def __init__(self, data_path, transfrom=None):
@@ -20,11 +9,11 @@ class MyCustomFSDD(Dataset):
         self.transform = transfrom
 
     def __getitem__(self, index):
+
         fn = self.data_path[index]
-        sample_rate, wave = wavfile.read(fn)
-        #wave = torch.from_numpy(wave)
-        digit = int(fn.split("\\")[-1].split("_")[0])
-        speaker = fn.split("\\")[-1].split("_")[1]
+        wave, sample_rate = torchaudio.load_wav(fn)
+        digit = int(fn.split("/")[-1].split("_")[0])
+        speaker = fn.split("/")[-1].split("_")[1]
 
         if self.transform:
             wave = self.transform(wave)
@@ -33,7 +22,3 @@ class MyCustomFSDD(Dataset):
 
     def __len__(self):
         return len(self.data_path)
-
-
-
-
---
+++ code_examples/learn-spec.py
@@ -0,0 +1,86 @@
+# Basic imports
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+
+# PyTorch imports
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torchaudio import transforms
+
+# Dataset class and neural network class
+from fsdd_dataset import MyCustomFSDD
+import models
+
+# Learning parameters
+batch_size = 1
+learning_rate = 1e-3
+L1, L2, L3 = (250, 100, 40)
+max_epoch = 5
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+# Dataset variables
+folder_data = glob.glob("/home/fatih/free-spoken-digit-dataset/recordings/*.wav")
+classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+
+# Preprocessing variables
+n_fft = 1024
+fbins = n_fft//2 + 1
+
+# Output files
+netname = "net.pth"
+
+# Create the Spectrogram transform
+spec_transform = transforms.Spectrogram(n_fft=n_fft)
+
+# Initialize the dataset and dataloader
+traindataset = MyCustomFSDD(folder_data, transfrom=spec_transform)
+trainloader = DataLoader(traindataset, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=0)
+
+# Initialize the NN model
+net = models.SpectrogramLearner(fbins=fbins, output_dim=10, hidden_dims=(L1, L2, L3), batch_size=batch_size)
+net = net.to(device)
+
+# Optimizer and Loss function
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), learning_rate, momentum=0.9)
+
+# Training
+for epoch in range(max_epoch):
+    dataiter = 0
+    
+    for batch in trainloader:
+        dataiter += 1
+        spec = batch[0][0].squeeze(0).squeeze(0).to(device)
+        label = batch[1]
+
+        h1 = torch.zeros(batch_size, L1, requires_grad=True)
+        h2 = torch.zeros(batch_size, L2, requires_grad=True)
+
+        c1 = torch.zeros(batch_size, L1, requires_grad=True)
+        c2 = torch.zeros(batch_size, L2, requires_grad=True)
+
+        
+        for i in range(spec.size()[1]):
+
+            state = [h1, c1 ,h2, c2]
+            y_pred, last_state = net(spec[:, i].unsqueeze(0), state)
+
+            h1 = torch.tensor(last_state[0])
+            h2 = torch.tensor(last_state[2])
+            c1 = torch.tensor(last_state[1])
+            c2 = torch.tensor(last_state[3])
+
+            optimizer.zero_grad()
+            loss = criterion(y_pred, label)
+            
+            loss.backward()
+            optimizer.step()
+            
+            print("Epoch:",epoch+1, "Data-Num:",dataiter, "Time-Segment:", i, "Loss:",loss.item())
+
+print("Saving trained model...")
+torch.save(net.state_dict(), "./" + netname)
