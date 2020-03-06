f3f469ecd32217930c717ed6d08e675d8aed6a2f
Mehmet Kapson <56561848+Bladrach@users.noreply.github.com>
2020-03-06 23:34:00 +0300

    GPU implementation added

--- code_examples/fsdd/CNN/try2guess.py
+++ code_examples/fsdd/CNN/try2guess.py
@@ -1,13 +1,14 @@
 from learn_spec_img import *
 
 
+device = torch.device('cuda')
 net.load_state_dict(torch.load("./saved_models/net_epoch_16.pth"))
 img = cv2.imread('0_mehmet_0.png')
 print(img.shape)
 img = np.transpose(img, (2, 0, 1))
 print(img.shape)
 img = torch.from_numpy(img)
-pred = net(img[0, :, :].unsqueeze(0).unsqueeze(0))
+pred = net(img[0, :, :].unsqueeze(0).unsqueeze(0).to(device))
 print(pred.size())
 _, predicted = torch.max(pred.data, 1)
 
