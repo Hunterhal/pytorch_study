3ac73d6fe7a3bddf38e6e950ac3bab12020773e0
Mehmet Kapson <56561848+Bladrach@users.noreply.github.com>
2020-03-06 23:39:35 +0300

    Update try2guess.py

--- code_examples/fsdd/CNN/try2guess.py
+++ code_examples/fsdd/CNN/try2guess.py
@@ -1,7 +1,7 @@
 from learn_spec_img import *
 
 
-device = torch.device('cuda')
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 net.load_state_dict(torch.load("./saved_models/net_epoch_16.pth"))
 img = cv2.imread('0_mehmet_0.png')
 print(img.shape)
