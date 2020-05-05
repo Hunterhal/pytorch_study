ae104de1be5f261b85a8fb2c9e305b8766ae54f3
Mehmet Kapson <56561848+Bladrach@users.noreply.github.com>
2020-05-05 17:07:58 +0300

    Update models.py

--- code_examples/fsdd/CNN/models.py
+++ code_examples/fsdd/CNN/models.py
@@ -4,23 +4,20 @@ import torch.nn.functional as F
 
 
 # Model used in learn_spec_img.py
+# fft 64 33x33 lük resimler için save
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 5, 5, padding = 0)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(5, 13, 5, padding = 0)
-        self.fc1 = nn.Linear(4901, 1200)
-        self.fc2 = nn.Linear(1200, 200)
-        self.fc3 = nn.Linear(200, 50)
-        self.fc4 = nn.Linear(50, 10)
+        self.conv2 = nn.Conv2d(5, 7, 5, padding = 0)
+        self.fc1 = nn.Linear(175, 50)
+        self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x.float())))
         x = self.pool(F.relu(self.conv2(x.float())))
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = self.fc4(x)
-        return x    
+        x = self.fc2(x)
+        return x
