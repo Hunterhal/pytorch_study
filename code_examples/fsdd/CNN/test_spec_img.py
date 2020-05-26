eaf10c635ccc854eabcf032b4f41f6c36efa5470
Mehmet Kapson <56561848+Bladrach@users.noreply.github.com>
2020-05-26 16:25:50 +0300

    Update test_spec_img.py

--- code_examples/fsdd/CNN/test_spec_img.py
+++ code_examples/fsdd/CNN/test_spec_img.py
@@ -2,8 +2,7 @@ from learn_spec_img import *
 import matplotlib.pyplot as plt
 
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-directory = "./saved_models"
+directory = "./33x33_saved_models"
 files = list(filter(os.path.isfile, glob.glob(directory + "/*.pth")))
 files.sort(key=lambda x: os.path.getmtime(x))
 acc_list = []
@@ -15,21 +14,26 @@ for filename in files:
     print(filename)
     with torch.no_grad():
         for data in testloader:
-            spec = np.transpose(data[0], (0, 3, 1, 2))
-            spec = spec.to(device)
+            spec = data[0].unsqueeze(1).to(device)
             label = data[1].to(device)
-            output = net(spec[:, 0, :, :].unsqueeze(1))
+            output = net(spec)
 
             _, predicted = torch.max(output.data, 1)
             total += label.size(0)
             correct += (predicted == label).sum().item()
 
-    acc_list.append(100*(correct/total))
+    acc_list.append(100 * (correct/total))
     epoch_list.append(int(filename.split("_")[-1].split(".")[0]))
-    
+
+elapsedTimeFile = open('elapsed_time.txt')
+elapsedTime = float(elapsedTimeFile.read())
+elapsedTimeFile.close()
+
 fig = plt.figure()
 plt.plot(epoch_list, acc_list)
+plt.title("Elapsed Time ({} minutes), Test Accuracy ({:.3f} %)".format(float(elapsedTime), float(acc_list[-1])))
+plt.grid()
 plt.xlabel("Epoch")
-plt.ylabel("Accuracy(%)")
+plt.ylabel("Accuracy (%)")
 fig.savefig("TrainingHistory.jpg")
 plt.show()
